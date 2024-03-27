817ce43c52053b505f7c474e89214644eb62aa0e
Ole Kirchner <ole_kirchner@gmx.de>
2024-03-27 13:42:30 +0100

    Updated year and changed Gatsby Cloud to Netlify

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,13 @@ import { ArrowUpRight } from 'lucide-react'
 
 export default function Footer() {
   return (
-    <div className="flex h-32 flex-col items-center justify-center border-t border-neutral-200 px-6 dark:border-neutral-700 md:h-16 md:flex-row md:justify-between md:px-8 xl:px-12">
+    <div className="flex h-32 flex-col items-center justify-center border-t border-neutral-200 px-4 dark:border-neutral-700 sm:px-6 md:h-16 md:flex-row md:justify-between md:px-8 xl:px-12">
       <div className="text-center">
         Built using
         <a
           href="https://www.gatsbyjs.com/"
           target="blank"
-          className="ml-1 text-black underline transition hover:text-neutral-500 dark:text-white dark:hover:text-neutral-400"
+          className="ml-1 mr-1 text-black underline transition hover:text-neutral-500 dark:text-white dark:hover:text-neutral-400"
         >
           <span className="whitespace-nowrap">
             Gatsby.js
@@ -19,14 +19,28 @@ export default function Footer() {
             />
           </span>
         </a>
-        , deployed on
+        and
         <a
-          href="https://www.gatsbyjs.com/products/cloud/"
+          href="https://tailwindcss.com/"   
           target="blank"
           className="ml-1 text-black underline transition hover:text-neutral-500 dark:text-white dark:hover:text-neutral-400"
         >
           <span className="whitespace-nowrap">
-            Gatsby Cloud
+            TailwindCSS
+            <ArrowUpRight
+              size={16}
+              className="relative top-[-1px] left-[1px] inline"
+            />
+          </span>
+        </a>
+        , deployed on
+        <a
+          href="https://www.netlify.com/"
+          target="blank"
+          className="ml-1 text-black underline transition hover:text-neutral-500 dark:text-white dark:hover:text-neutral-400"
+        >
+          <span className="whitespace-nowrap">
+            Netlify
             <ArrowUpRight
               size={16}
               className="relative top-[-1px] left-[1px] inline"
@@ -35,7 +49,7 @@ export default function Footer() {
         </a>
       </div>
 
-      <div className="mt-2 md:mt-0">©2023 Ole Kirchner</div>
+      <div className="mt-2 md:mt-0">© Ole Kirchner / 2024</div>
     </div>
   )
 }
